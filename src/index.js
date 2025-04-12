be5e72a042d4ddcad3a69af25d07c8e0605d7c62
refraynn <80609634+refraynn@users.noreply.github.com>
2025-04-12 02:49:37 -0700

    Slash Commands
    Added slash command functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
 import 'dotenv/config';
+import { REST } from '@discordjs/rest';
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const TOKEN = process.env.BOT_TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
 
 
 const client = new Client({ 
@@ -9,13 +14,38 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ] 
 });
-// say hi to sarah 2
+
+const rest = new REST({ version: '10' }).setToken(TOKEN)
  
 client.on('ready', () => {
     console.log(`RGPB is online.`);
   });
 
-client.on('messageCreate', (message) => {
-    console.log(message.content);
+client.on('interactionCreate', (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        console.log('Hello world')
+        interaction.reply({ content: 'Hello!'})
+    }
 })
-client.login(process.env.BOT_TOKEN);
+
+async function main() {
+    const commands = [
+        {
+            name: 'blahblahblah',
+            description: 'blahblahblah'
+        },
+    ];
+
+    try {
+        console.log('Started refreshing slash commands.');
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands,
+        })
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main();
+client.login(TOKEN);
