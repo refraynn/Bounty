0e068081d489cf9f26ce9b9bbaa8ad940cc1e226
refraynn <80609634+refraynn@users.noreply.github.com>
2025-04-12 18:19:29 -0700

    Command Handler (Broken!)
    TypeError: eventFolders is not iterable

---
+++ src/events/ready/online.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+    console.log(`${client.user.tag} is online.`)
+};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import { Client, GatewayIntentBits, Routes } from 'discord.js';
 import 'dotenv/config';
 import { REST } from '@discordjs/rest';
 
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const TOKEN = process.env.BOT_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
+import eventHandler from './handlers/eventHandler.js'
 
 
 const client = new Client({ 
@@ -16,12 +18,6 @@ const client = new Client({
 });
 
  
-client.on('ready', () => {
-    console.log(`RGPB is online.`);
-  });
+eventHandler(client);
 
-
-
-
-main();
 client.login(TOKEN);
